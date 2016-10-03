2466bd03fd0803ad7a8c84f7145d9d21eae5687c
Karl Redgate <Karl.Redgate@gmail.com>
2016-10-03 16:42:59 -0400

    fix requires

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
-module.exports = require( './lib/vicarious' );
-module.exports.prototype.methods.HEAD = require( './lib/head' );
-module.exports.prototype.methods.GET = require( './lib/get' );
-module.exports.prototype.methods.PUT = require( './lib/put' );
-module.exports.prototype.methods.POST = require( './lib/post' );
-module.exports.prototype.methods.PROPFIND = require( './lib/propfind' );
-module.exports.prototype.methods.PROPPATCH = require( './lib/proppatch' );
-module.exports.prototype.methods.OPTIONS = require( './lib/options' );
-module.exports.prototype.methods.DELETE = require( './lib/delete' );
+module.exports = require( './vicarious' );
+module.exports.prototype.methods.HEAD = require( './methods/head' );
+module.exports.prototype.methods.GET = require( './methods/get' );
+module.exports.prototype.methods.PUT = require( './methods/put' );
+module.exports.prototype.methods.POST = require( './methods/post' );
+module.exports.prototype.methods.PROPFIND = require( './methods/propfind' );
+module.exports.prototype.methods.PROPPATCH = require( './methods/proppatch' );
+module.exports.prototype.methods.OPTIONS = require( './methods/options' );
+module.exports.prototype.methods.DELETE = require( './methods/delete' );
 
 // vim:autoindent expandtab sw=4 syntax=javascript
